1ce7ee7d2c777fb2405db423682b68daeb001544
Simon Boyer <simon.boyer@openwide.fr>
2013-04-18 12:19:48 +0200

    Tri owbootstrap / owskeletor

--- design/owbootstrap/templates/cols/center.tpl
+++
@@ -1,5 +0,0 @@
-<div class="span{$width} center-col">
-	<!-- Main area: START -->
-    {include uri='design:page_mainarea.tpl'}
-    <!-- Main area: END -->
-</div>
--- settings/design.ini.append.php
+++ settings/design.ini.append.php
@@ -4,18 +4,6 @@
 [ExtensionSettings]
 DesignExtensions[]=owbootstrap
 
-[Pagelayout]
-top_path_tpl=page_toppath.tpl
-header_tpl=page_header.tpl
-top_menu_tpl=menu/double_top.tpl
-left_col_tpl=cols/left.tpl
-left_col_width=3
-center_col_tpl=cols/center.tpl
-center_col_width=7
-right_col_tpl=cols/right.tpl
-right_col_width=2
-footer_tpl=page_footer.tpl
-
 [StylesheetSettings]
 # A list of stylesheets to use for the editor content.
 # Enable the code button in ezoe.ini to be able to see the
@@ -28,7 +16,6 @@ EditorCSSFileList[]=bootstrap-responsive.css
 
 CSSFileList[]=bootstrap.css
 CSSFileList[]=bootstrap-responsive.css
-CSSFileList[]=owbootstrap.css
 
 [JavaScriptSettings]
 JavaScriptList[]=ezjsc::jquery
